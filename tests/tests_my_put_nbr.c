08f7edab840ae3553708a1fd4b1c8b63a6efdb39
QG <quentin.gondrand@epitech.eu>
2021-10-06 14:05:50 +0200

    8/9

---
+++ tests/tests_my_put_nbr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** Day03
+** File description:
+** tests_my_put_nbr
+*/
+
+my_put_nbr(int nb);
+
+int main ()
+{
+    my_put_nbr(0) ;
+    my_put_nbr(42) ;
+    my_put_nbr((-2147483647) ;
+    return (0) ;
+}
